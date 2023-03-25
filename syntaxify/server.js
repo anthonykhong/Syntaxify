d5fc9bf5049579190bc443dd77e3195925ef4127
username <youremail@domain.com>
2023-03-24 17:18:08 -0700

    Step One Of Google Authentication

--- syntaxify/server.js
+++ syntaxify/server.js
@@ -1,14 +1,15 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-// var session = require('express-session');
-// var passport = require('passport');
-// var methodOverride = require('method-override');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
 
-// require("dotenv").config();
-require("./config/database");
+require('dotenv').config();
+require('./config/database');
+require('./config/passport');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -27,13 +28,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(methodOverride("_method"));
 
-// app.use(session({
-//   secret: process.env.SECRET,
-//   resave: false,
-//   saveUninitialized: true
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.use(function (req, res, next) {
 //   res.locals.user = req.user;

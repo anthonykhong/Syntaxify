a03ddb10c5a996c7cac4a6e845369c10c9950af5
kendall-johnson <kdotjohnson2019@gmail.com>
2023-03-28 14:57:34 -0700

    Implement Add Database/Framework/Language Feature

--- syntaxify/routes/documentations.js
+++ syntaxify/routes/documentations.js
@@ -17,15 +17,18 @@ const ensureLoggedIn = require("../config/ensureLoggedIn");
 router.get("/languages", documentationsCtrl.indexLanguages);
 router.get("/languages/new", ensureLoggedIn, documentationsCtrl.newLanguage);
 router.get("/languages/:id", documentationsCtrl.showLanguage);
+router.post("/languages", ensureLoggedIn, documentationsCtrl.createLanguage);
 
 // Routes for frameworks
 router.get("/frameworks", documentationsCtrl.indexFrameworks);
 router.get("/frameworks/new", ensureLoggedIn, documentationsCtrl.newFramework);
 router.get("/frameworks/:id", documentationsCtrl.showFramework);
+router.post("/frameworks", ensureLoggedIn, documentationsCtrl.createFramework);
 
 // Routes for databasFramework.Frarouter.get("/databases", documentationsCtrl.indexDatabases);
 router.get("/databases", documentationsCtrl.indexDatabases);
 router.get("/databases/new", ensureLoggedIn, documentationsCtrl.newDatabase);
 router.get("/databases/:id", documentationsCtrl.showDatabase);
+router.post("/databases", ensureLoggedIn, documentationsCtrl.createDatabase);
 
 module.exports = router;

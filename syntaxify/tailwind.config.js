7b8fe04d01d7efd412c72830dbebeaf24855bc55
anthonykhong <anthonykhong95200@gmail.com>
2023-03-30 04:12:36 +0200

    finish styling for index pages

--- syntaxify/tailwind.config.js
+++ syntaxify/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./views/*", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
   },
   plugins: [require("flowbite/plugin")],
 };

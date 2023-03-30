26e06e62d4997a4b0ec2168a1b6009b92cfdfedd
AllanMorales94 <overlydedicated1994@gmail.com>
2023-03-30 15:47:54 -0700

    Implement Img Model And Display

--- data.js
+++ data.js
@@ -3,43 +3,41 @@ exports.languages = [
     title: "HTML",
     content:
       "HTML, or Hypertext Markup Language, is a markup language used for creating and structuring content on web pages. It is the foundation of the World Wide Web and is used to create everything from simple static websites to complex web applications. HTML uses various elements and tags to define the structure, layout, and content of a web page, including headings, paragraphs, lists, links, images, and multimedia. HTML is often combined with other web technologies such as CSS and JavaScript to create interactive and engaging web pages.",
-      image: "",
+      image: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_640.png",
   },
   {
     title: "CSS",
     content:
       "CSS, or Cascading Style Sheets, is a style sheet language used to define the visual appearance and layout of HTML documents. CSS allows web developers to separate the presentation of a web page from its structure, making it easier to create and maintain consistent and visually appealing websites. CSS is used to define the font styles, colors, layout, spacing, and other visual elements of a web page, and can be applied to individual elements or across entire pages. CSS works by using selectors to target specific HTML elements and then applying styles and properties to those elements. By separating the presentation from the structure, CSS allows web developers to create flexible and dynamic web pages that can adapt to different screen sizes and devices.",
-      image: "",
   },
   {
     title: "JavaScript",
     content:
       "JavaScript is a high-level programming language used to create dynamic and interactive web pages. JavaScript allows developers to add interactivity, behavior, and functionality to web pages, such as validating user input, creating animations, and updating content without requiring a page refresh. JavaScript can be used on both the client-side and server-side of web development. On the client-side, JavaScript is used to manipulate the Document Object Model (DOM) of a web page, allowing developers to dynamically modify the content and structure of a page in response to user actions or other events. On the server-side, JavaScript can be used with Node.js to create server-side applications and perform tasks such as handling form submissions and interacting with databases. JavaScript is a popular language for web development due to its versatility, ease of use, and extensive library of tools and frameworks.",
-      image: "",
   },
   {
     title: "Python",
     content:
       "Python is a high-level, interpreted programming language used for a wide range of applications, from web development to scientific computing, data analysis, and artificial intelligence. Python's syntax is designed to be simple, easy to read, and easy to write, making it a popular choice for beginners and experts alike. Python is an object-oriented language that supports multiple programming paradigms, including procedural, functional, and object-oriented programming. Python's extensive library of modules and packages makes it a powerful tool for data manipulation, scientific computing, and machine learning. Python is also known for its strong community support, with a vast array of resources and tools available for developers.",
-      image: "",
+      
   },
   {
     title: "C#",
     content:
       "C# (pronounced C sharp) is a modern, object-oriented programming language developed by Microsoft. C# is a general-purpose language that is widely used for developing a variety of applications, including desktop software, video games, web applications, and mobile apps. C# is designed to be easy to learn and use, with a simple syntax that is similar to other C-style languages like C++ and Java. C# is known for its strong type safety and memory management, making it a safe and reliable language for building large and complex applications. C# is also known for its extensive library of built-in functions and support for modern programming paradigms, such as asynchronous programming and functional programming. C# is widely used in the .NET ecosystem, and is the primary language used to develop applications for the Windows operating system.",
-      image: "",
+      
   },
   {
     title: "Java",
     content:
       "Java is a popular, high-level programming language that is designed to be portable, robust, and secure. Java is a general-purpose language that can be used to create a wide range of applications, including desktop software, web applications, mobile apps, and even embedded systems. Java is an object-oriented language, meaning that it is built around the concept of objects, which can be thought of as self-contained units of code that encapsulate data and functionality. Java is also known for its write once, run anywhere philosophy, which means that Java code can be compiled into bytecode that can run on any platform that has a Java Virtual Machine (JVM). This makes Java a popular choice for cross-platform development. Java is also known for its strong community support, with a vast array of libraries, tools, and frameworks available for developers.",
-      image: "",
+      
   },
   {
     title: "Swift",
     content:
       "Swift is a modern, high-level programming language developed by Apple for iOS, macOS, watchOS, tvOS, and Linux platforms. Swift is designed to be fast, safe, and expressive, making it a popular choice for developing applications for Apple's platforms. Swift is an open-source language that is designed to be easy to learn and use, with a syntax that is similar to other modern programming languages like Python and Ruby. Swift is an object-oriented language that supports modern programming paradigms, such as functional programming and protocol-oriented programming. Swift is known for its type safety and memory management, making it a reliable language for building large and complex applications. Swift is also known for its strong interoperability with Objective-C, allowing developers to use both languages within the same project. The availability of Swift Playgrounds, a fun and interactive environment for learning Swift, has also helped to make the language more accessible to new developers.",
-      image: "",
+      
   },
 ];
 exports.frameworks = [
@@ -47,43 +45,43 @@ exports.frameworks = [
     title: "Node.js",
     content:
       "Node.js is a popular, open-source, cross-platform JavaScript runtime environment that allows developers to build server-side applications using JavaScript. Node.js is built on top of the V8 JavaScript engine used in Google Chrome, and provides an event-driven, non-blocking I/O model that makes it highly efficient and scalable. Node.js is often used for building real-time web applications, microservices, and APIs, and is popular in the development of serverless architectures. Node.js has a vast ecosystem of packages and modules available through the Node Package Manager (npm), making it easy for developers to leverage existing code and libraries to build their applications. Node.js also has a strong community of developers, with many resources and tools available for developers to learn and use the technology.",
-      image: "",
+      
   },
   {
     title: "Express.js",
     content:
       "Express.js, commonly referred to simply as Express, is a popular, open-source web application framework for Node.js. Express provides a simple and flexible set of features for building web applications and APIs, including routing, middleware, and templating. Express is known for its minimalist and unopinionated approach to web development, allowing developers to build applications using their preferred libraries and tools. Express is highly customizable, allowing developers to choose the components they need and leave out the ones they don't. Express also has a large ecosystem of plugins and extensions available through the Node Package Manager (npm), making it easy for developers to add functionality to their applications. Express is widely used in the Node.js community and is a popular choice for building web applications and APIs due to its simplicity, flexibility, and ease of use.",
-      image: "",
+      
   },
   {
     title: "jQuery",
     content:
       "jQuery is a fast, small, and feature-rich JavaScript library designed to simplify client-side scripting of HTML. jQuery is designed to make it easier to navigate and manipulate the HTML Document Object Model (DOM) by providing a simple and concise syntax for common tasks such as event handling, animation, and AJAX interactions. jQuery is widely used by developers to simplify and streamline the process of developing web applications and has a large community of users who contribute to its development and provide support to other users. jQuery is compatible with all modern browsers and is a popular choice for building interactive and dynamic user interfaces. jQuery can be easily integrated with other popular web development technologies like HTML, CSS, and JavaScript frameworks like AngularJS, ReactJS, and Vue.js.",
-      image: "",
+      
   },
   {
     title: "Angular",
     content:
       "Angular is a popular, open-source, front-end web application framework developed and maintained by Google. Angular is designed to simplify and streamline the process of building complex web applications by providing a comprehensive set of tools and features for building dynamic, single-page applications. Angular is built on top of HTML, CSS, and JavaScript and provides a modular architecture that allows developers to easily organize and maintain their code. Angular includes features like two-way data binding, dependency injection, and a powerful templating system, making it easy to build rich, interactive user interfaces. Angular also has a large ecosystem of modules and packages available through the Node Package Manager (npm), making it easy for developers to add functionality to their applications. Angular is widely used by developers to build complex, data-driven web applications and has a strong community of users who contribute to its development and provide support to other users.",
-      image: "",
+      
   },
   {
     title: "React",
     content:
       "React, also known as ReactJS, is an open-source, front-end JavaScript library for building user interfaces. Developed and maintained by Facebook, React is designed to simplify the process of building complex, interactive user interfaces by providing a component-based architecture that allows developers to easily reuse and compose UI elements. React is known for its declarative and efficient approach to programming, which allows developers to describe how the UI should look based on the application's state. React also provides a virtual DOM, which allows for fast and efficient updates to the UI without the need to re-render the entire page. React can be used in conjunction with other JavaScript libraries and frameworks and is often paired with Redux for managing state in complex applications. React has a large and active community of developers and is widely used in the development of web applications and mobile apps.",
-      image: "",
+      
   },
   {
     title: "Ruby on Rails",
     content:
       "Ruby on Rails, often simply called Rails, is an open-source, full-stack web application framework written in the Ruby programming language. Rails is designed to simplify and accelerate the process of building web applications by providing a set of conventions and tools for common web development tasks, such as database management, routing, and rendering views. Rails emphasizes the use of a Model-View-Controller (MVC) architectural pattern, which helps to separate the application's concerns and improve maintainability. Rails also emphasizes the use of convention over configuration, allowing developers to focus on writing application code rather than boilerplate configuration. Rails has a large and active community of developers, with many plugins and extensions available through the RubyGems package manager. Rails is widely used by developers to build a variety of web applications, from small startups to large-scale enterprise applications.",
-      image: "",
+      
   },
   {
     title: "Django",
     content:
       "Django is a popular, open-source, high-level Python web application framework designed for rapid development and clean, pragmatic design. Developed and maintained by the Django Software Foundation, Django provides a comprehensive set of tools and features for building web applications, including database management, URL routing, authentication, and template rendering. Django follows the Model-View-Controller (MVC) architectural pattern and emphasizes the use of reusable code and don't repeat yourself (DRY) principles. Django is known for its robustness, security, and scalability, making it a popular choice for building complex, data-driven web applications. Django also has a large and active community of developers, with many plugins and extensions available through the Python Package Index (PyPI). Django is widely used by developers to build a variety of web applications, from small startups to large-scale enterprise applications.",
-      image: "",
+      
   },
 ];
 exports.databases = [
@@ -91,18 +89,18 @@ exports.databases = [
     title: "MongoDB",
     content:
       "MongoDB is an open-source, document-oriented NoSQL database system designed for scalability, performance, and ease of development. MongoDB uses a flexible document model, allowing developers to store data in a way that closely matches the structure of the application data. MongoDB is designed to work well with unstructured and semi-structured data, making it a good choice for applications that handle large volumes of data with varying structures. MongoDB also provides a number of features for scalability and high availability, including automatic sharding and replica sets. MongoDB can be used with a variety of programming languages and has a large and active community of developers, with many plugins and extensions available. MongoDB is widely used in web development, especially in applications that require flexible data modeling and scalability.",
-      image: "",
+      
   },
   {
     title: "MySQL",
     content:
       "MySQL is an open-source, relational database management system (RDBMS) designed for scalability, reliability, and performance. MySQL is one of the most popular RDBMS in the world and is used by many web applications and websites, including some of the world's largest and most popular sites. MySQL supports a wide range of programming languages, including PHP, Java, Python, and others, and provides a comprehensive set of tools and features for managing data, including SQL queries, transactions, and stored procedures. MySQL is known for its high performance, scalability, and reliability, making it a popular choice for web developers who require a powerful and stable database solution. MySQL is also highly customizable and can be used with a wide range of hardware and software configurations, from small web applications to large-scale enterprise systems. MySQL is an important part of the LAMP stack (Linux, Apache, MySQL, PHP/Python/Perl), which is one of the most popular web development stacks in use today.",
-      image: "",
+      
   },
   {
     title: "PostgreSQL",
     content:
       "PostgreSQL, also known as Postgres, is a powerful, open-source, object-relational database management system (ORDBMS) designed for performance, reliability, and data integrity. PostgreSQL uses a client/server architecture and is known for its advanced features, including support for complex data types, full-text search, and spatial data. PostgreSQL provides a comprehensive set of tools and features for managing data, including SQL queries, transactions, and stored procedures, and supports a wide range of programming languages, including Python, Java, C++, and many others. PostgreSQL is designed to be highly extensible and customizable, and provides many features for improving performance and scalability, such as multi-version concurrency control (MVCC) and asynchronous replication. PostgreSQL is used by many web applications and is a popular choice for developers who require a powerful, flexible, and reliable database system.",
-      image: "",
+      
   },
 ];
